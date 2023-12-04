3eabea5f456e81fc09a43b1e3a028043f557cdb6
Miguel <migueldelcorso@gmail.com>
2023-12-04 17:49:14 -0300

    Added gradle task to create new files for the new days

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,34 @@ tasks {
     wrapper {
         gradleVersion = "8.5"
     }
+    task("nextDay") {
+        doLast {
+            val prevDay = fileTree("$projectDir/src")
+                .matching { include("Day*.kt") }
+                .maxOf {
+                    val (prevDay) = Regex("Day(\\d\\d)").find(it.name)!!.destructured
+                    prevDay.toInt()
+                }
+            val currDay = String.format("%02d", prevDay + 1)
+            File("$projectDir/src", "Day$currDay.kt").writeText(
+                """
+private fun part1(input: List<String>): Int {
+    return 0
+}
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val testInput = readInput("${currDay}_test")
+    check(part1(testInput) == 0)
+    
+    val input = readInput("$currDay")
+    part1(input).println()
+    part2(input).println()
+}
+"""
+            )
+        }
+    }
 }
