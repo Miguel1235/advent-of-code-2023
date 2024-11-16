360f5fe46879cf7bf19b23f595ca1b68c3991fc2
Miguel Del Corso <migueldelcorso@gmail.com>
2024-11-16 10:40:01 -0300

    Not finished day 16, still refactoring

--- src/Day16.kt
+++ src/Day16.kt
@@ -2,98 +2,168 @@ private enum class Dirs {
     LEFT, RIGHT, UP, DOWN
 }
 
+private data class Position(val r: Int, val c: Int)
+
 private data class Beam(
-    val moving: Dirs,
-    var positions: MutableList<Pair<Int, Int>> = mutableListOf(Pair(1, 0)),
+    val movingInDirectionOf: Dirs = Dirs.RIGHT,
+    var positions: MutableList<Position> = mutableListOf(Position(0, 0)),
+    var currentItem: Char = '-',
+    var isActive: Boolean = true,
 )
 
-private class PoweredGrid {
-    val activeBeams = mutableListOf(Beam(Dirs.RIGHT))
-    val inactiveBeams = mutableListOf<Beam>()
+private class PoweredGrid(val maxR: Int, val maxC: Int) {
+    val beams = mutableListOf(Beam(Dirs.RIGHT, currentItem = '-'))
+    fun splitBean(draw: Char, r: Int, c: Int): List<Beam> {
+        val newBeams = mutableListOf<Beam>()
+        for (beam in beams) {
+            println("The last position of the beam was ${beam.positions.last()} and the split is on $r-$c")
 
-    fun moveBeans(r: Int, c: Int) {
-        for (currentBeam in activeBeams) {
-            val lastPosition = currentBeam.positions[currentBeam.positions.lastIndex]
-            when (currentBeam.moving) {
-                Dirs.LEFT -> {
-                    if (lastPosition.first == r) {
-                        currentBeam.positions.add(Pair(lastPosition.first, lastPosition.second - 1))
+            val lastBeamPosition = beam.positions.last()
+
+            if (lastBeamPosition.r == r && lastBeamPosition.c == c) {
+                if (beam.currentItem == '-' && draw == '|') {
+                    // TODO: check if its possible to add a beam on that new position because of grid constraints
+                    if(r-1 >= 0) {
+                        newBeams.add(Beam(Dirs.UP, mutableListOf(Position(r - 1, c)), currentItem = draw))
                     }
+
+                    newBeams.add(Beam(Dirs.DOWN, mutableListOf(Position(r + 1, c)), currentItem = draw))
+                    beam.isActive = false
+                }
+            }
+        }
+        return newBeams
+    }
+
+
+    fun moveBeam(beam: Beam) {
+
+        val lastP = beam.positions.last()
+        if(!beam.isActive) return
+        when (beam.movingInDirectionOf) {
+                Dirs.LEFT -> {
+//                    if (lastPosition.r == r) {
+//                        currentBeam.positions.add(Pair(lastPosition.first, lastPosition.second - 1))
+//                    }
                 }
 
                 Dirs.RIGHT -> {
-                    if (lastPosition.first == r) {
-                        currentBeam.positions.add(Pair(lastPosition.first, lastPosition.second + 1))
+                    println("Moving beam to ->")
+                    beam.positions.add(Position(lastP.r, lastP.c + 1))
+                    if(lastP.c + 1 == maxC) {
+                        println("Going to desactivate the beam because it reached the end")
+                        beam.isActive = false
                     }
                 }
 
                 Dirs.UP -> {
-                    if (lastPosition.second == c) {
-                        currentBeam.positions.add(Pair(lastPosition.first - 1, lastPosition.second))
-                    }
+                    println("Moving up")
+//                    if (lastPosition.second == c) {
+//                        currentBeam.positions.add(Pair(lastPosition.first - 1, lastPosition.second))
+//                    }
                 }
 
                 Dirs.DOWN -> {
-                    if (lastPosition.second == c) {
-                        currentBeam.positions.add(Pair(lastPosition.first + 1, lastPosition.second))
+                    println("Moving v")
+
+
+                    beam.positions.add(Position(lastP.r+1, lastP.c))
+
+                    if(lastP.r + 1 == maxR) {
+                        println("Going to desactivate the beam because it reached the end of the row")
+                        beam.isActive = false
                     }
                 }
             }
-        }
-    }
-
-    fun splitBean(draw: Char) {
-        val total = activeBeams.size
-        for(i in 0..<total) {
-            val beam = activeBeams[i]
-            val lastPosition = beam.positions[beam.positions.lastIndex]
-            inactiveBeams.add(beam)
-            activeBeams.removeLast()
-            activeBeams.add(Beam(Dirs.UP, mutableListOf(Pair(lastPosition.first - 1, lastPosition.second))))
-            activeBeams.add(Beam(Dirs.DOWN, mutableListOf(Pair(lastPosition.first + 1, lastPosition.second))))
-        }
-    }
-
-    override fun toString(): String {
-        return "ActiveBeams: $activeBeams - InactiveBeams: $inactiveBeams"
     }
 
 }
 
 private fun part1(grid: List<String>): Int {
-    val poweredGrid = PoweredGrid()
+    val maxR = grid.size
+    val maxC = grid[0].length
 
-    for (i in grid.indices) {
-        val row = grid[i]
-        for (j in row.indices) {
-            if (i == 0 && j == 0) continue
-            val item = row[j]
-            when (item) {
-                '/' -> {
-                    println("Right Mirror")
-                }
+    val poweredGrid = PoweredGrid(maxR, maxC)
 
-                '\\' -> {
-                    println("Left Mirror")
-                }
 
-                '-' -> {
-                    println("Horizontal splitter")
-                }
+    var activeBeams = 1
 
-                '|' -> {
-                    println("Vertical splitter")
-                    poweredGrid.splitBean('|')
-                }
+//    while(activeBeams >= 1) {
+//
+//        activeBeams = poweredGrid.beams.fold(0) {acc, beam -> acc + if(beam.isActive) 1 else 0}
+//
+//        println("Number of active beams: $activeBeams")
+//        val beam = poweredGrid.beams.first { beam -> beam.isActive }
+//        val lastP = beam.positions.last()
+//
+//        if (beam.currentItem == '-') {
+//            for (i in lastP.c until maxC) {
+//                println("The beam is on the position ${lastP.r}-$i")
+//                val item = grid[lastP.r][i]
+//                when (item) {
+//                    '/' -> {
+//                        println("/")
+//                    }
+//
+//                    '\\' -> {
+//                        println("|")
+//                    }
+//
+//                    '-' -> {
+//                        println("-")
+//                    }
+//
+//                    '|' -> {
+//                        println("|")
+//                        val newBeams = poweredGrid.splitBean('|', lastP.r, i)
+//                        println("The new beams to add are: $newBeams")
+//
+////                        println("Beams: ${poweredGrid.beams}")
+////                        println("NewBeams: $newBeams")
+//                        poweredGrid.beams.addAll(newBeams)
+//                    }
+//                    else -> {
+//                        println(".")
+//                        println("Moving the beam to the left/right")
+//                        poweredGrid.moveBeam(beam)
+//                    }
+//                }
+//            }
+//        } else {
+//            // The beam is |
+//            println("The beam is |")
+//
+//            for (i in lastP.r until maxR) {
+//                println("The beam is on the position ${lastP.r}-$i")
+//                val item = grid[lastP.r][i]
+//                println("The item on the grid is $item")
+//                when (item) {
+//                    '/' -> {
+//                        println("/")
+//                    }
+//
+//                    '\\' -> {
+//                        println("|")
+//                    }
+//
+//                    '-' -> {
+//                        println("-")
+//                        val newBeams = poweredGrid.splitBean('|', lastP.r, i)
+//                        println("The new beams to add are: $newBeams")
+//                        poweredGrid.beams.addAll(newBeams)
+//                    }
+//
+//                    else -> {
+//                        println("|")
+//                        poweredGrid.moveBeam(beam)
+//                    }
+//                }
+//            }
+//
+//        }
+//    }
 
-                else -> {
-                    println("space")
-                    poweredGrid.moveBeans(i, j)
-                }
-            }
-        }
-    }
-    println(poweredGrid)
+//    println(poweredGrid)
     return 0
 }
 
